5708ae9e235c6d89405367a69f76f5f9977de80a
katherinheb <katherinheb@gmail.com>
2024-11-19 13:38:24 -0500

    Update GMultipleDatatype.java

--- src/GMultipleDatatype.java
+++ src/GMultipleDatatype.java
@@ -1,9 +1,26 @@
+public class GMultipleDatatype  <Datatypeone, DatatypeTwo>  {
+   Datatypeone valueOne;
+   DatatypeTwo valueTwo;
 
-public class GMultipleDatatype {
+   public GMultipleDatatype(Datatypeone v1, DatatypeTwo v2)
+   {
+       this.valueOne = v1;
+       this.valueTwo = v2;
+   }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   public Datatypeone getValueOne() {
+       return valueOne;
+   }
 
-	}
+   public void setValueOne(Datatypeone valueOne) {
+       this.valueOne = valueOne;
+   }
 
+   public DatatypeTwo getValueTwo() {
+       return valueTwo;
+   }
+
+   public void setValueTwo(DatatypeTwo valueTwo) {
+       this.valueTwo = valueTwo;
+   }
 }
